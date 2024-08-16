699dd34dd02b92d064f966f693d006a9cbf14e37
hershjoshii <110454248+hershjoshii@users.noreply.github.com>
2024-08-15 23:01:32 -0400

    Update nav.py

--- app/src/modules/nav.py
+++ app/src/modules/nav.py
@@ -13,11 +13,11 @@ def AboutPageNav():
 def PolStratAdvHomeNav():
     st.sidebar.page_link("pages/00_Pol_Strat_Home.py", label="Political Strategist Home", icon='👤')
 
-def WorldBankVizNav():
-    st.sidebar.page_link("pages/01_World_Bank_Viz.py", label="World Bank Visualization", icon='🏦')
+#def WorldBankVizNav():
+#    st.sidebar.page_link("pages/01_World_Bank_Viz.py", label="World Bank Visualization", icon='🏦')
 
-def MapDemoNav():
-    st.sidebar.page_link("pages/02_Map_Demo.py", label="Map Demonstration", icon='🗺️')
+#def MapDemoNav():
+#    st.sidebar.page_link("pages/02_Map_Demo.py", label="Map Demonstration", icon='🗺️')
 
 def GetVideos():
     st.sidebar.page_link("pages/04_Get_Videos.py", label="Get Videos", icon='🗺️')
@@ -32,13 +32,13 @@ def NewTagNav():
 def PredictionNav():
     st.sidebar.page_link("pages/11_Prediction.py", label="Regression Prediction", icon='📈')
 
-def ClassificationNav():
-    st.sidebar.page_link("pages/13_Classification.py", label="Classification Demo", icon='🌺')
+#def ClassificationNav():
+#    st.sidebar.page_link("pages/13_Classification.py", label="Classification Demo", icon='🌺')
 
 #### ------------------------ System Admin Role ------------------------
 def AdminPageNav():
     st.sidebar.page_link("pages/20_Admin_Home.py", label="System Admin", icon='🖥️')
-    st.sidebar.page_link("pages/21_ML_Model_Mgmt.py", label='ML Model Management', icon='🏢')
+#    st.sidebar.page_link("pages/21_ML_Model_Mgmt.py", label='ML Model Management', icon='🏢')
     st.sidebar.page_link("pages/22_Get_Users.py", label='Get all Users', icon='🏢')
 
 #### ------------------------ System Admin Role ------------------------
@@ -70,16 +70,16 @@ def SideBarLinks(show_home=False):
         # Show World Bank Link and Map Demo Link if the user is a political strategy advisor role.
         if st.session_state['role'] == 'pol_strat_advisor':
             PolStratAdvHomeNav()
-            WorldBankVizNav()
+            #WorldBankVizNav()
             NewTagNav()
-            MapDemoNav()
+        #    MapDemoNav()
 
         # If the user role is usaid worker, show the Api Testing page
         if st.session_state['role'] == 'usaid_worker':
             PredictionNav()
             ApiTestNav() 
             NewTagNav()
-            ClassificationNav()
+        #    ClassificationNav()
         
         # If the user is an administrator, give them access to the administrator pages
         if st.session_state['role'] == 'administrator':
