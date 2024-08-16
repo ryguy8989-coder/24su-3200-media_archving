c5c40f6bfa4073e245896d02caf11b589882c1a4
Ryan Jiang <jiang.ry@northeastern.edu>
2024-08-16 13:01:23 -0400

    Update 12_View_Literature.py

--- app/src/pages/12_View_Literature.py
+++ app/src/pages/12_View_Literature.py
@@ -16,28 +16,3 @@ try:
   st.dataframe(lit)
 except:
   st.write("Could not connect to database to get literature")
-
-#If you want to run this, change DB_name in .env
-#products = requests.get('http://api:4000/p/products').json()
-#try:
-#  st.dataframe(products)
-#except:
-#  st.write("Could not connect to database to get products")
-
-"""
-Simply retrieving data from a REST api running in a separate Docker Container.
-
-If the container isn't running, this will be very unhappy.  But the Streamlit app 
-should not totally die. 
-"""
-'''
-
-data = {} 
-try:
-  data = requests.get('http://api:4000/data').json()
-except:
-  st.write("**Important**: Could not connect to sample api, so using dummy data.")
-  data = {"a":{"b": "123", "c": "hello"}, "z": {"b": "456", "c": "goodbye"}}
-
-st.dataframe(data)
-'''
